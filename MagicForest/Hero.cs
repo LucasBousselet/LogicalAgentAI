11c7133a0eb7771efd04eab0f9f25370b77a936b
Lucas <BousseletLucas@gmail.com>
2016-12-04 17:03:02 -0500

    Creation of userControl "ForestCell" Adding of some images needed for the project (monster, hero, hole, ...) Creation of the class "Hero" which includes several attributes and basic methods

---
+++ MagicForest/Hero.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicForest
+{
+    class Hero
+    {
+        private bool lightSensor;
+
+        private bool stenchSensor;
+
+        private bool windSensor;
+
+        public int score;
+
+        public Hero()
+        {
+            this.lightSensor = false;
+            this.stenchSensor = false;
+            this.windSensor = false;
+            this.score = 0;
+        }
+
+        public bool HasLight
+        {
+            get
+            {
+                return lightSensor;
+            }
+            set
+            {
+                lightSensor = value;
+            }
+        }
+
+        public bool HasStench
+        {
+            get
+            {
+                return stenchSensor;
+            }
+            set
+            {
+                stenchSensor = value;
+            }
+        }
+
+        public bool HasWind
+        {
+            get
+            {
+                return windSensor;
+            }
+            set
+            {
+                windSensor = value;
+            }
+        }
+
+        public void ThrowRock()
+        {
+
+        }
+    }
+}
